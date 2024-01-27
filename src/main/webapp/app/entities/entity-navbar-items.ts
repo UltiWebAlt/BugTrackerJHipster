a79a5e95904efd01cb9af3909231fff0c4d54a7a
Randy Burgess <rb@ultiweb.com>
2024-01-27 02:24:47 +0000

    Updating your backend tutorial step fails Liquibase validation

--- src/main/webapp/app/entities/entity-navbar-items.ts
+++ src/main/webapp/app/entities/entity-navbar-items.ts
@@ -1,3 +1,19 @@
 import NavbarItem from 'app/layouts/navbar/navbar-item.model';
 
-export const EntityNavbarItems: NavbarItem[] = [];
+export const EntityNavbarItems: NavbarItem[] = [
+  {
+    name: 'Project',
+    route: '/project',
+    translationKey: 'global.menu.entities.project',
+  },
+  {
+    name: 'Label',
+    route: '/label',
+    translationKey: 'global.menu.entities.label',
+  },
+  {
+    name: 'Ticket',
+    route: '/ticket',
+    translationKey: 'global.menu.entities.ticket',
+  },
+];
